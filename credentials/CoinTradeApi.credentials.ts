8963145789b12c21d49863555a9d77509a118658
L啊 <liaoyux@gmail.com>
2024-09-22 15:16:18 +0800

    api test

--- credentials/CoinTradeApi.credentials.ts
+++ credentials/CoinTradeApi.credentials.ts
@@ -1,7 +1,14 @@
 import {
 	ICredentialType,
 	INodeProperties,
+	ICredentialDataDecryptedObject,
+	ICredentialTestRequest,
+	// IAuthenticate,
+	IHttpRequestOptions
 } from 'n8n-workflow';
+import exchanges from '../nodes/CoinTrade/exchanges';
+
+const TEST_URL_FLAG = 'https://example.com' // for test purpose, any site who response 200
 
 export class CoinTradeApi implements ICredentialType {
 	name = 'coinTradeApi';
@@ -72,6 +79,15 @@ export class CoinTradeApi implements ICredentialType {
 				}
 			}
 		},
+		{
+			displayName: 'api proxy',
+			name: 'proxy',
+			type: 'string',
+			default: '',
+			required: false,
+			placeholder: 'e.g. socks://user:password@ip:port',
+			description: "provide proxy to test whether api is correct only"
+		},
 		// {
 		// 	displayName: 'api uid',
 		// 	name: 'uid',
@@ -85,4 +101,28 @@ export class CoinTradeApi implements ICredentialType {
 		// 	}
 		// }
 	];
+
+	async authenticate (credentials: ICredentialDataDecryptedObject, requestOptions: IHttpRequestOptions): Promise<IHttpRequestOptions> {
+		if (requestOptions.baseURL === TEST_URL_FLAG) {
+			try {
+				const exchange = await exchanges.get(credentials.platform as string)
+				exchanges.setKeys(exchange, credentials.apiKey as string,  credentials.secret as string, credentials.password as string)
+				exchanges.setProxy(exchange, credentials.proxy as string);
+				await exchange.fetchBalance()
+				exchanges.setProxy(exchange, '');
+				exchanges.clearKeys(exchange)
+			}
+			catch (e) {
+				requestOptions.baseURL = 'https://api.airtable.com/v0/meta/whoami' // for test purpose, put any site who should return error
+				return requestOptions
+			}
+		}
+		return requestOptions;
+	}
+	test: ICredentialTestRequest = {
+		request: {
+			baseURL: TEST_URL_FLAG,
+			timeout: 5000
+		}
+	};
 }
